e06e4c02dd8dea76300539094be589cd52c8b6d9
Jadië Oliveira <jadieoliveira00@gmail.com>
2020-08-23 10:03:41 -0300

    Initial Commit

---
+++ hi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hi
+//
+//  Created by Jadiê Oliveira on 23/08/20.
+//  Copyright © 2020 Jadiê Oliveira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
