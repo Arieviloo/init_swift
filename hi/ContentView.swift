58e776d953f4f7d569963da63a98531d9b7de5af
Jadië Oliveira <jadieoliveira00@gmail.com>
2020-08-23 10:12:23 -0300

    hi

--- hi/ContentView.swift
+++ hi/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Ei, Dev!")
+                .font(.title)
+                .foregroundColor(.purple)
+            Text("Meu nome é Jadiê Oliveira, programador front end e iniciando no mundo swift.")
+        }.padding(20)
     }
 }
 
